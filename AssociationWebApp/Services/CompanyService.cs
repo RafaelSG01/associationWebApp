79bdded5cd5b1ca98900a5cbd56f81669e74bb7c
Rafael Soares <rafaelsoares01@gmail.com>
2020-11-09 11:41:49 -0300

    Incluiding companyService

---
+++ AssociationWebApp/Services/CompanyService.cs
@@ -0,0 +1,37 @@
+﻿using AssociationWebApp.Data;
+using AssociationWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssociationWebApp.Services
+{
+    public class CompanyService
+    {
+        private readonly AssociationDbContext _context;
+
+        public CompanyService(AssociationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Company>> FainBayNameAsync(string name, string cnpj)
+        {
+            var result = from obj in _context.Company select obj;
+            if (!String.IsNullOrEmpty(name))
+            {
+                result = result.Where(x => x.Company.Name.Contains(name));
+            }
+
+            if (!String.IsNullOrEmpty(cnpj))
+            {
+                result = result.Where(x => x.Cnpj.Contains(cnpj));
+            }
+
+
+            return await result.ToListAsync();
+        }
+    }
+}
